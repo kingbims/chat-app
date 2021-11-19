8623b4fde8005df27d68babcfc77951a38484431
Moremi <moremim@nameksolutions.com>
2021-11-19 11:19:10 +0100

    init commit

---
+++ src/utils/messages.js
@@ -0,0 +1,21 @@
+const getMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+const getLocationMessage = (username, url) => {
+    return {
+        username,
+        url,
+        createdAt: new Date().getTime()
+    }
+}
+
+
+module.exports = {
+    getMessage,
+    getLocationMessage
+}
